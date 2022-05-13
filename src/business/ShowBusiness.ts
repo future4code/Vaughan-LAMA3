1100f972f909ab907fc0be38ca3c996df6315f8d
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-14 03:24:51 +0800

    5 e 6

--- src/business/ShowBusiness.ts
+++ src/business/ShowBusiness.ts
@@ -1,8 +1,91 @@
-import { BaseDatabase } from "../data/BaseDatabase";
+import { ShowDatabase } from "../data/ShowDatabase";
+import { DAY, Show, ShowDate, ShowInputDTO } from "./model/Show";
+import { IdGenerator } from "./services/IdGenerator";
 
 
-export class ShowBusiness { 
+export class ShowBusiness {
   constructor(
-      private showDatabase : BaseDatabase
-  ){}
+    private showDatabase: ShowDatabase,
+    private idGenerator : IdGenerator
+  ) { }
+  public signShow = async (input: ShowInputDTO) => {
+
+    // const { bandId, weekDay, startTime, endTime } = input
+
+    // if (!bandId || !weekDay || !startTime || !endTime) {
+    //   throw new Error("Please fill all the fields!");
+    // }
+    // if (startTime < 8 || startTime > 22 || endTime < 9 || endTime > 23) {
+    //   throw new Error("That's out of our show hours!");
+    // }
+    // if (weekDay.toLowerCase().trim() !== "friday" &&weekDay.toLowerCase().trim() !== "saturday" && weekDay.toLowerCase().trim() !== "sunday") {
+    //   throw new Error("That's out of our show hours!");
+    // }
+    const { bandId, weekDay, startTime, endTime } = this.checks(input)
+
+    const id = this.idGenerator.generateId();
+
+    const show: Show = {
+      id,
+      bandId, 
+      weekDay, 
+      startTime, 
+      endTime
+    }
+        
+      const showsInthatTime = await this.showDatabase.checkTimeSlot(show)
+     
+      if(showsInthatTime.length > 0 ) { 
+        throw new Error("This slot is not available!");
+      }
+   
+       await this.showDatabase.insertShow(show)
+
+
+
+    //  const showArray : Show[]=[]
+    //     let j = endTime - startTime
+    //   //  for( let i = startTime ; i <= endTime  ;  i++ ){
+    //    for( let i = startTime ; i <= endTime-1  ;  i++ ){
+    //       // const a = {id , bandId , weekDay , startTime : i, endTime , durantion : j-- }
+    //       const id = this.idGenerator.generateId()
+    //       const a = {id , bandId , weekDay , startTime : i, endTime  }
+    //       showArray.push(a)           
+    //    }
+    //    console.log(showArray)
+    //   await this.showDatabase.insertShow(showArray)
+
+  }
+  public  gettingShowByDate = async(input: DAY )=> {
+    
+    const Date : ShowDate =  {
+    weekDay: input,
+    startTime: 8,
+    endTime: 23,
+    }
+
+    const result = this.showDatabase.gettingShowByDate(Date)
+     
+    
+
+
+   
+  }
+  public checks =(input :ShowInputDTO )=> { 
+    const { bandId, weekDay, startTime, endTime } = input
+
+    if (!bandId || !weekDay || !startTime || !endTime) {
+      throw new Error("Please fill all the fields!");
+    }
+    if (startTime < 8 || startTime > 22 || endTime < 9 || endTime > 23) {
+      throw new Error("That's out of our show hours!");
+    }
+    if (weekDay.toLowerCase().trim() !== "friday" &&weekDay.toLowerCase().trim() !== "saturday" && weekDay.toLowerCase().trim() !== "sunday") {
+      throw new Error("That's out of our show hours!");
+    }
+
+    return input;
+  }
+
+
 }
--- src/data/ShowDatabase.ts
+++ src/data/ShowDatabase.ts
@@ -1,6 +1,79 @@
+import { Show, ShowDate, ShowDateDTO, ShowOutDTO } from "../business/model/Show";
 import { BaseDatabase } from "./BaseDatabase";
 
 
-export class ShowDatabase extends BaseDatabase{ 
-    
+export class ShowDatabase extends BaseDatabase {
+
+    public insertShow = async (show: Show) => {
+        try {
+
+            const showDb: ShowOutDTO = (
+                {
+                    id: show.id,
+                    week_day: show.weekDay,
+                    start_time: show.startTime,
+                    end_time: show.endTime,
+                    band_id: show.bandId
+                })
+
+            await BaseDatabase.connection("lama_shows")
+                .insert(showDb)
+        } catch (error: any) {
+            throw new Error(error.sqlmessage || error.message);
+
+        }
+    }
+    public checkTimeSlot = async (show: Show) => {
+        try {
+
+            const showDb: ShowOutDTO = (
+                {
+                    id: show.id,
+                    week_day: show.weekDay,
+                    start_time: show.startTime,
+                    end_time: show.endTime,
+                    band_id: show.bandId
+                })
+
+
+            return BaseDatabase.connection("lama_shows")
+                .where({ week_day: show.weekDay })
+                .havingBetween('start_time', [show.startTime, show.endTime])
+
+        } catch (error: any) {
+            throw new Error(error.sqlmessage || error.message);
+
+        }
+    }
+    public gettingShowByDate = async (data: ShowDate) => {
+        try {
+
+            const showByDate : ShowDateDTO =
+            {
+                week_day: data.weekDay,
+                start_time: data.startTime,
+                end_time: data.endTime,
+            }
+
+
+            const result = await  BaseDatabase.connection("lama_shows")
+            .join("lama_bands","lama_bands.id","=","lama_shows.band_id")
+            .where({ week_day: showByDate.week_day })
+            .havingBetween('start_time', [showByDate.start_time, showByDate.end_time])
+
+            return   result.map(item => {
+                 return { 
+                     name: item.name,
+                     musicGenre: item.music_genre
+                 }
+             })
+            
+            
+            
+       
+
+        } catch (error: any) {
+            throw new Error(error.sqlmessage || error.message);
+        }
+    }
 }
